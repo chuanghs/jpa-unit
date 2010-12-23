bb2343697779fa918e34ae3a88968a35c9d59278
krzyzak <krzyzak@PLWAR0273.sys.pl.intra.net>
2010-12-23 12:32:22 +0100

    properties reading initial test

---
+++ jpaunit-core/src/test/java/org/jpaunit/JPAUnitPropertiesTest.java
@@ -0,0 +1,22 @@
+package org.jpaunit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: krzyzak
+ * Date: 23.12.10
+ * Time: 17:28
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class JPAUnitPropertiesTest {
+
+    @Test
+    public void testReadProperties(){
+
+    }
+
+}
