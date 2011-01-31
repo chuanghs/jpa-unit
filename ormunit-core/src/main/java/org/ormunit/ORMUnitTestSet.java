62d24730d7f5dde4ec475b16d0befafa746277ab
tomasz krzyzak <tomasz.krzyzak@gmail.com>
2011-01-31 10:15:45 +0100

    1) unignored

---
+++ ormunit-core/src/main/java/org/ormunit/ORMUnitTestSet.java
@@ -0,0 +1,56 @@
+package org.ormunit;
+
+import org.ormunit.command.ORMUnitCommand;
+import org.ormunit.node.INodeProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Tomasz Krzyzak
+ * Date: 12.12.10
+ * Time: 20:56
+ */
+public class ORMUnitTestSet {
+
+    private static final Logger log = LoggerFactory.getLogger(ORMUnitTestSet.class);
+
+    private final List<ORMUnitCommand> commands = new LinkedList<ORMUnitCommand>();
+
+    private final ORMProvider provider;
+
+    public ORMUnitTestSet(ORMProvider provider) {
+        this.provider = provider;
+    }
+
+    public void addCommand(ORMUnitCommand command) {
+        commands.add(command);
+    }
+
+
+    public void execute() {
+        for (ORMUnitCommand command : commands) {
+            command.visit(provider);
+        }
+    }
+
+    public ORMProvider getProvider() {
+        return provider;
+    }
+
+
+    private Map<String, INodeProcessor> nodeProcessorMap = new HashMap<String, INodeProcessor>();
+
+    public INodeProcessor getNodeProcessor(String nodeName) {
+        return this.nodeProcessorMap.get(nodeName);
+    }
+
+    public void registerNodeProcessor(String nodeType, INodeProcessor iNodeProcessor) {
+        nodeProcessorMap.put(nodeType, iNodeProcessor);
+    }
+}
