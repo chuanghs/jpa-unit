9ce9063d843f76e97cdb73e0ae0eb8245c3cbd53
tomasz.krzyzak <tomasz.krzyzak@localhost>
2012-01-02 00:13:24 +0100

    jpa example project

---
+++ example/src/main/java/org/ormunit/example/Employee.java
@@ -0,0 +1,31 @@
+package org.ormunit.example;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@IdClass(EmployeeId.class)
+public class Employee {
+
+    @Id
+    @Column(name = "E_ID")
+    private Integer eId;
+
+    @Id
+    private String country;
+
+    @OneToMany(mappedBy = "contact")
+    private List<PhoneNumber> contactNumber;
+
+    public List<PhoneNumber> getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Integer geteId() {
+        return eId;
+    }
+}
---
+++ example/src/main/java/org/ormunit/example/EmployeeId.java
@@ -0,0 +1,19 @@
+package org.ormunit.example;
+
+import java.io.Serializable;
+
+public class EmployeeId implements Serializable {
+
+    private Integer eId;
+    private String country;
+
+
+    public EmployeeId() {
+
+    }
+
+    public EmployeeId(String country, Integer eId) {
+        this.country = country;
+        this.eId = eId;
+    }
+}
